c44b6fb44ec75f89761132f6a91f4199127ae681
Geo <alfmilan23@gmail.com>
2018-11-26 16:16:27 -0800

    "Revised Vault Page, Documents, and Events Pages"

--- frontend/src/components/Documents/Documents.js
+++ frontend/src/components/Documents/Documents.js
@@ -1,10 +1,56 @@
 import React, { Component } from 'react';
+import "./Documents.css";
 
 class Documents extends Component {
     render(){
         return(
-            <div><h1>Documents Page</h1></div>
+        <div>
+            <div className ="big-box">
+                <div class="jumbotron">
+                    <h1>(Not Final Description) PWelcome to the Documents page!</h1>
+                    <p>Please browse through our collection of 9000+ Documents!</p>
+                </div>
+            </div>
+        <div>
+         
+         <div id="container">
+            <div className ="filter-box">
+                <div class="FlexCon">
+                    <div class="FlexIt">Filter</div>
+                        
+                </div>
+            </div>
+
+            <div className ="documents">
+                <div class="FlexContainer">
+                  <div class="FlexItem">Item 1</div>
+                  <div class="FlexItem">Item 2</div>
+                  <div class="FlexItem">Item 3</div>
+                  <div class="FlexItem">Item 4</div>
+                  <div class="FlexItem">Item 5</div>
+                  <div class="FlexItem">Item 6</div>
+                  <div class="FlexItem">Item 7</div>
+                  <div class="FlexItem">Item 8</div>
+                  <div class="FlexItem">Item 9</div>
+                  <div class="FlexItem">Item 10</div>
+                  <div class="FlexItem">Item 11</div>
+                  <div class="FlexItem">Item 12</div>
+                  <div class="FlexItem">Item 13</div>
+                  <div class="FlexItem">Item 14</div>
+                  <div class="FlexItem">Item 15</div>
+                  <div class="FlexItem">Item 16</div>
+                  <div class="FlexItem">Item 17</div>
+                  <div class="FlexItem">Item 18</div>
+                  <div class="FlexItem">Item 19</div>
+                  <div class="FlexItem">Item 20</div>
+                </div>
+            </div>
+            </div>
+
+                </div> 
+              </div>
         )
+        
     }
 }
 
