7b9f94b2e40dd8efea33fffcdb1c2a4ea30562fa
Wellford <wmchan@ucsc.edu>
2018-11-27 15:08:48 -0800

    created gail_data.json and updated homepage

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -3,21 +3,15 @@ import './Home.css';
 class Home extends Component {
     render(){
         return(
-        <div>
-            <div className="home-container">
-                <div className="home-left">
-                    <h1>Okinawa Memory Initiative</h1>
-                    <p>An international, public history project that explores the founding years of the American military occupation of Okinawa.</p>
-                </div>
-                <div className="home-center"></div>
-                <div className="home-right">
-                    {/* <img className="home-map" src="https://www.welt-atlas.de/datenbank/karten/karte-6-865.gif"/> */}
-                </div>
+        <div className="home-container">
+            <div className="home-left">
             </div>
-            <div>
-                <h1>Featured Items</h1>
-
-
+            <div className="home-center">
+                <h1>Okinawa Memory Initiative</h1>
+                <p>An international, public history project that explores the founding years of the American military occupation of Okinawa.</p>
+            </div>
+            <div className="home-right">
+                {/* <img className="home-map" src="https://www.welt-atlas.de/datenbank/karten/karte-6-865.gif"/> */}
             </div>
         </div>
         )
