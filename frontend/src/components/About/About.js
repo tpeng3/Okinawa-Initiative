a74f37cae3148c4d61107d2a65e79f3c13a1c651
Tina Peng <tpeng3@ucsc.edu>
2018-12-08 18:12:01 -0800

    adjusted flexboxes for about icons

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -20,9 +20,9 @@ class About extends Component {
         
             <h1>The Founders</h1>
             <div class="row">
-                <div class="column"><img alt="icon" src={require('./Drew_Richardson.jpeg')} onmouseover="this.src='icon2.png';" onmouseout="this.src='icon.png';" style={iconstyle}/><div class="name">MEMBER NAME</div></div>
-                <div class="column"><img alt="icon" src={require('./Drew_Richardson.jpeg')} onmouseover="this.src='icon2.png';" onmouseout="this.src='icon.png';" style={iconstyle}/><div class="name">MEMBER NAME</div></div>
-                <div class="column"><img alt="icon" src={require('./Drew_Richardson.jpeg')} onmouseover="this.src='icon2.png';" onmouseout="this.src='icon.png';" style={iconstyle}/><div class="name">MEMBER NAME</div></div>
+                <div class="column1"><img alt="icon" src={require('./Drew_Richardson.jpeg')} onmouseover="this.src='icon2.png';" onmouseout="this.src='icon.png';" style={iconstyle}/><div class="name">MEMBER NAME</div></div>
+                <div class="column1"><img alt="icon" src={require('./Drew_Richardson.jpeg')} onmouseover="this.src='icon2.png';" onmouseout="this.src='icon.png';" style={iconstyle}/><div class="name">MEMBER NAME</div></div>
+                <div class="column1"><img alt="icon" src={require('./Drew_Richardson.jpeg')} onmouseover="this.src='icon2.png';" onmouseout="this.src='icon.png';" style={iconstyle}/><div class="name">MEMBER NAME</div></div>
             </div>
         
             <h1>Our Story</h1>
@@ -31,37 +31,37 @@ class About extends Component {
             <h1>The Professionals</h1>
             <p id="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <div className="row">
-            <div className="column"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
-            <div className="column"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
-            <div className="column"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
-            <div className="column"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
+            <div className="column2"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
+            <div className="column2"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
+            <div className="column2"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
+            <div className="column2"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
             </div>
         
             <h1>The Team</h1>
             <p id="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <div className="row">
-            <div className="column"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
-            <div className="column"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
-            <div className="column"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
-            <div className="column"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
+            <div className="column2"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
+            <div className="column2"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
+            <div className="column2"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
+            <div className="column2"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
             </div>
         
             <h1>The Alumni</h1>
             <p id="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <div className="row">
-            <div className="column"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
-            <div className="column"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
-            <div className="column"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
-            <div className="column"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
+            <div className="column2"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
+            <div className="column2"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
+            <div className="column2"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
+            <div className="column2"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
             </div>
         
             <h1>The Sponsors</h1>
             <p id="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <div className="row">
-            <div className="column"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
-            <div className="column"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
-            <div className="column"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
-            <div className="column"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
+            <div className="column2"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
+            <div className="column2"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
+            <div className="column2"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
+            <div className="column2"><img alt="icon" src={require('./Drew_Richardson.jpeg')} style={iconstyle}/></div>
             </div>
         
             </div>
