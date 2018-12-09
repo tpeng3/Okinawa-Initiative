39244220b01bb4dc442f696ef00020e5bcd0ce14
Tina Peng <tpeng3@ucsc.edu>
2018-12-08 17:04:38 -0800

    fixed video's css page

--- frontend/src/components/Videos/video_detail.js
+++ frontend/src/components/Videos/video_detail.js
@@ -12,11 +12,11 @@ const VideoDetail = (props) => {
     const url = `https://www.youtube.com/embed/${videoId}`;
 
     return (
-        <div className="video-detail col-md-8">
+        <div class="video">
             <div className="embed-responsive embed-responsive-16by9">
                 <iframe className="embed-responsive-item" src={url}></iframe>
             </div>
-            <div className="details">
+            <div className="viddetails">
                 <div>{video.snippet.title}</div>
                 <div>{video.snippet.description}</div>
             </div>
